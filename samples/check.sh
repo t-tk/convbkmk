fad52cfb574844cc91a207b80809be17fe6aea3c
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2018-11-10 23:31:48 +0900

    More test

--- samples/check.sh
+++ samples/check.sh
@@ -32,13 +32,21 @@ $convbkmk -O bkmk-up-dpps-ov.ps && diff -q $testdir/bkmk-up-dpps-convbkmk.ps bkm
 
 # with dvipdfmx
 
+nkf -e $testdir/bkmk-p-dpdx_bak.out > bkmk-p-dpdx-euc.out
+nkf -s $testdir/bkmk-p-dpdx_bak.out > bkmk-p-dpdx-sjis.out
 cp $testdir/bkmk-p-dpdx_bak.out bkmk-p-dpdx.out
 cp $testdir/bkmk-up-dpdx_bak.out bkmk-up-dpdx.out
 cp $testdir/bkmk-docinfo_bak.out bkmk-docinfo.out
 
-$convbkmk -o bkmk-p-dpdx.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx.out || rc=101
-$convbkmk -o bkmk-up-dpdx.out && diff -q $testdir/bkmk-up-dpdx.out bkmk-up-dpdx.out || rc=101
-$convbkmk -o bkmk-docinfo.out && diff -q $testdir/bkmk-docinfo.out bkmk-docinfo.out || rc=103
+$convbkmk -e -o bkmk-p-dpdx-euc.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx-euc.out || rc=101
+$convbkmk -s -o bkmk-p-dpdx-sjis.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx-sjis.out || rc=102
+$convbkmk -eo bkmk-p-dpdx-euc.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx-euc.out || rc=103
+$convbkmk -so bkmk-p-dpdx-sjis.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx-sjis.out || rc=104
+$convbkmk --enc=e -o bkmk-p-dpdx-euc.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx-euc.out || rc=105
+$convbkmk --enc=s -o bkmk-p-dpdx-sjis.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx-sjis.out || rc=106
+$convbkmk -o bkmk-p-dpdx.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx.out || rc=107
+$convbkmk -o bkmk-up-dpdx.out && diff -q $testdir/bkmk-up-dpdx.out bkmk-up-dpdx.out || rc=108
+$convbkmk -o bkmk-docinfo.out && diff -q $testdir/bkmk-docinfo.out bkmk-docinfo.out || rc=109
 
 exit $rc
 
