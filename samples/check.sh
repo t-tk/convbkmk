34bd63c9121e127ba5aaa104a2712740ec89c2e3
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2018-11-10 20:16:28 +0900

    check.sh: updated

--- samples/check.sh
+++ samples/check.sh
@@ -4,34 +4,41 @@
 # You may freely use, modify and/or distribute this file.
 
 testdir=./test
+convbkmk=../convbkmk.rb
 
 rc=0
 
+nkf -e $testdir/bkmk-p-dpps.ps > bkmk-p-dpps-euc.ps
+nkf -s $testdir/bkmk-p-dpps.ps > bkmk-p-dpps-sjis.ps
+cp $testdir/bkmk-up-dpps.ps bkmk-up-dpps.ps
+cp $testdir/bkmk-up-dpps.ps bkmk-up-dpps-ov.ps
+
 # with dvips
-nkf -e $testdir/bkmk-p-dpps.ps > bkmk-p-dpps-euc.ps && convbkmk -e bkmk-p-dpps-euc.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-euc-convbkmk.ps || rc=1
 
-nkf -s $testdir/bkmk-p-dpps.ps > bkmk-p-dpps-sjis.ps && convbkmk -s bkmk-p-dpps-sjis.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-sjis-convbkmk.ps || rc=2
+$convbkmk -e bkmk-p-dpps-euc.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-euc-convbkmk.ps || rc=1
+$convbkmk -s bkmk-p-dpps-sjis.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-sjis-convbkmk.ps || rc=2
+$convbkmk -u bkmk-up-dpps.ps && diff -q $testdir/bkmk-up-dpps-convbkmk.ps bkmk-up-dpps-convbkmk.ps || rc=3
 
-cp $testdir/bkmk-up-dpps.ps bkmk-up-dpps.ps && convbkmk -u bkmk-up-dpps.ps && diff -q $testdir/bkmk-up-dpps-convbkmk.ps bkmk-up-dpps-convbkmk.ps || rc=3
+$convbkmk -g bkmk-p-dpps-euc.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-euc-convbkmk.ps || rc=4
+$convbkmk -g bkmk-p-dpps-sjis.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-sjis-convbkmk.ps || rc=5
+$convbkmk -g bkmk-up-dpps.ps && diff -q $testdir/bkmk-up-dpps-convbkmk.ps bkmk-up-dpps-convbkmk.ps || rc=6
 
-nkf -e $testdir/bkmk-p-dpps.ps > bkmk-p-dpps-euc.ps && convbkmk -g bkmk-p-dpps-euc.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-euc-convbkmk.ps || rc=4
+$convbkmk --enc=e bkmk-p-dpps-euc.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-euc-convbkmk.ps || rc=7
+$convbkmk --enc=s bkmk-p-dpps-sjis.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-sjis-convbkmk.ps || rc=8
+$convbkmk --enc=u bkmk-up-dpps.ps && diff -q $testdir/bkmk-up-dpps-convbkmk.ps bkmk-up-dpps-convbkmk.ps || rc=9
 
-nkf -s $testdir/bkmk-p-dpps.ps > bkmk-p-dpps-sjis.ps && convbkmk -g bkmk-p-dpps-sjis.ps && diff -q $testdir/bkmk-p-dpps-convbkmk.ps bkmk-p-dpps-sjis-convbkmk.ps || rc=5
-
-cp $testdir/bkmk-up-dpps.ps bkmk-up-dpps.ps && convbkmk -g bkmk-up-dpps.ps && diff -q $testdir/bkmk-up-dpps-convbkmk.ps bkmk-up-dpps-convbkmk.ps || rc=6
-
-cp $testdir/bkmk-up-dpps.ps bkmk-up-dpps-ov.ps && convbkmk -O bkmk-up-dpps-ov.ps && diff -q $testdir/bkmk-up-dpps-convbkmk.ps bkmk-up-dpps-ov.ps || rc=7
+$convbkmk -O bkmk-up-dpps-ov.ps && diff -q $testdir/bkmk-up-dpps-convbkmk.ps bkmk-up-dpps-ov.ps || rc=10
 
 
 # with dvipdfmx
 
-cp $testdir/bkmk-p-dpdx_bak.out bkmk-p-dpdx.out && convbkmk -o bkmk-p-dpdx.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx.out || rc=101
+cp $testdir/bkmk-p-dpdx_bak.out bkmk-p-dpdx.out
+cp $testdir/bkmk-up-dpdx_bak.out bkmk-up-dpdx.out
+cp $testdir/bkmk-docinfo_bak.out bkmk-docinfo.out
 
-cp $testdir/bkmk-up-dpdx_bak.out bkmk-up-dpdx.out && convbkmk -o bkmk-up-dpdx.out && diff -q $testdir/bkmk-up-dpdx.out bkmk-up-dpdx.out || rc=101
-
-cp $testdir/bkmk-docinfo_bak.out bkmk-docinfo.out && convbkmk -o bkmk-docinfo.out && diff -q $testdir/bkmk-docinfo.out bkmk-docinfo.out || rc=103
-
+$convbkmk -o bkmk-p-dpdx.out && diff -q $testdir/bkmk-p-dpdx.out bkmk-p-dpdx.out || rc=101
+$convbkmk -o bkmk-up-dpdx.out && diff -q $testdir/bkmk-up-dpdx.out bkmk-up-dpdx.out || rc=101
+$convbkmk -o bkmk-docinfo.out && diff -q $testdir/bkmk-docinfo.out bkmk-docinfo.out || rc=103
 
 exit $rc
-
 
